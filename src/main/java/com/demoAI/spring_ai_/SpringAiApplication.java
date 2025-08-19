072461df55abd22c6ec57c39340a5963d761720a
NguyenVuong-V <vuongtnnse184868@fpt.edu.vn>
2025-08-19 18:17:56 +0700

    Set the time zone

--- src/main/java/com/demoAI/spring_ai_/SpringAiApplication.java
+++ src/main/java/com/demoAI/spring_ai_/SpringAiApplication.java
@@ -7,6 +7,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringAiApplication {
 	public static void main(String[] args) {
+		// Set the timezone to Asia/Ho_Chi_Minh
+		// In runtime process I have a problem with PostgresSQL
+		// The time zone in my Window OS is set Asia/Saigon
+		// PostgresSQL is included Asia/Ho_Chi_Minh so that i need to set the time zone at run time
 		System.setProperty("user.timezone", "Asia/Ho_Chi_Minh");
 		SpringApplication.run(SpringAiApplication.class, args);
 	}
